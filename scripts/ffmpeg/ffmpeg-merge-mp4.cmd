05a24bcd60996f0e8bfc2ca7e797921c257c7f78
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2023-08-21 21:06:33 +0200

    Update ffmpeg-merge-mp4.cmd

--- scripts/ffmpeg/ffmpeg-merge-mp4.cmd
+++ scripts/ffmpeg/ffmpeg-merge-mp4.cmd
@@ -8,7 +8,7 @@ REM
 del "merged.mp4" 2> NUL:
 IF EXIST "merged.mp4" echo [ERROR] Stop, target file exists. & pause & goto :eof
 REM
-(FOR /R %%A IN (*.mp4) DO @ECHO file '%%A') | ffmpeg -protocol_whitelist file,pipe -f concat -safe 0 -i pipe: -vcodec copy -acodec copy "merged.mp4"
+(FOR /R %%A IN (*.mp4) DO @ECHO file '%%A') | sort | ffmpeg -protocol_whitelist file,pipe -f concat -safe 0 -i pipe: -vcodec copy -acodec copy "merged.mp4"
 REM
 timeout 3
 REM
