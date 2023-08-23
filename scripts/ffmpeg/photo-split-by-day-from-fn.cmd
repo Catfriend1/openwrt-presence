3ff203a640fda25b7ee5000f26f6bfb033e1741b
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2023-08-23 15:34:30 +0200

    Update photo-split-by-day-from-fn.cmd

--- scripts/ffmpeg/photo-split-by-day-from-fn.cmd
+++ scripts/ffmpeg/photo-split-by-day-from-fn.cmd
@@ -11,8 +11,14 @@ SET MERGE_MP4_FILES=1
 SET REENCODE_VIDEO=1
 SET ANALYSE_LOGS=1
 REM
+REM SET REENCODE_QUALITY=28
+SET REENCODE_QUALITY=30
+REM
+REM SET REENCODE_SCALE=hd1080
+SET REENCODE_SCALE=hd720
+REM
 IF "%REENCODE_VIDEO%" == "0" SET "FFMPEG_STREAM_MODE=-vcodec copy -acodec copy"
-IF NOT "%REENCODE_VIDEO%" == "0" SET "FFMPEG_STREAM_MODE=-filter:v "scale=hd1080,fps=30" -c:v libx265 -vtag hvc1 -crf 28 -c:a aac -b:a 192k"
+IF NOT "%REENCODE_VIDEO%" == "0" SET "FFMPEG_STREAM_MODE=-filter:v "scale=%REENCODE_SCALE%,fps=30" -c:v libx265 -vtag hvc1 -crf %REENCODE_QUALITY% -c:a aac -b:a 192k"
 REM MP3
 REM 	-c:a libmp3lame -b:a 192k
 REM
@@ -23,10 +29,11 @@ SET "SOURCE_FOLDER=%~dps0"
 SET "TARGET_FOLDER=%SOURCE_FOLDER%"
 REM
 IF NOT EXIST "%TARGET_FOLDER%" MD %TARGET_FOLDER% 2>NUL:
-FOR /F "delims=" %%A in ('DIR /B "%SOURCE_FOLDER%\*.*" 2^>NUL:') DO call :handleFile %%A
+IF "%MOVE_FILES_TO_SUBFOLDERS%" == "1" call :moveFilesToSubFolders
 REM
 IF "%MERGE_MP4_FILES%" == "1" call :handleSubDirs
 REM
+echo.
 echo [INFO] Collecting errors ...
 IF "%ANALYSE_LOGS%" == "1" grep -l -i "error" *.log > "files-with-errors.txt" & TYPE "files-with-errors.txt" 2>NUL:
 REM
@@ -62,8 +69,10 @@ REM
 echo [INFO] Merging dir [%HSDM_DIR%] into [%TARGET_MERGED_MP4_FULLFN%] ...
 (FOR /R %%A IN (%HSDM_DIR%\*.mp4) DO @ECHO file '%%A') | sort | ffmpeg -loglevel error -protocol_whitelist file,pipe -f concat -safe 0 -i pipe: %FFMPEG_STREAM_MODE% "%TARGET_MERGED_MP4_FULLFN%" > %TARGET_MERGED_LOG_FULLFN% 2>&1
 SET FFMPEG_ERRORLEVEL=%ERRORLEVEL%
+REM
+IF NOT "%FFMPEG_ERRORLEVEL%" == "0" echo [ERROR] ffmpeg FAILED, code #%FFMPEG_ERRORLEVEL%.
+TYPE %TARGET_MERGED_LOG_FULLFN% 2>NUL: | findstr /i "error" >NUL: && call :typeLoggedFirstErrors %TARGET_MERGED_LOG_FULLFN%
 IF NOT "%FFMPEG_ERRORLEVEL%" == "0" echo [ERROR] ffmpeg FAILED, code #%FFMPEG_ERRORLEVEL%. >> %TARGET_MERGED_LOG_FULLFN%
-IF NOT "%FFMPEG_ERRORLEVEL%" == "0" echo [ERROR] ffmpeg FAILED, code #%FFMPEG_ERRORLEVEL%. & goto :eof
 REM
 goto :eof
 
